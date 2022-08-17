b15add9826e872d611df88d89e86f6c59e652ba6
Raiyaxi-ZIaoi <82474669+Raiyaxi-Ziaoi@users.noreply.github.com>
2022-08-17 02:49:49 +0100

    Code added

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Countdown
+{
+    static class Extensions
+    {
+        public static string Scramble(this string s)
+        {
+            return new string(s.ToCharArray().OrderBy(x => Guid.NewGuid()).ToArray());
+        }
+
+        public static int LengthIEnumerable(this IEnumerable<string> sarr)
+        {
+            int length = 0;
+
+            foreach (string str in sarr)
+            {
+                length++;
+            }
+
+            return length;
+        }
+
+    }
+
+    static class Problems
+    {
+        static Random random = new Random();
+        const int ITERATIONS = 10;
+
+        public static bool AskWord()
+        {
+            string[] dictionary = System.IO.File.ReadAllLines(@"../../dict.txt");
+
+            // Random line chosen
+            string word = "";
+            while (true)
+            {
+                word = dictionary[random.Next(0, dictionary.Length)];
+                if (word.Length == 9)
+                {
+                    break;
+                }
+            }
+
+            string question_word = Extensions.Scramble(word);
+
+            Console.WriteLine("Word is: " + question_word);
+
+            // Answer
+            Console.WriteLine("\nAnswer: ");
+            string psbword = Console.ReadLine();
+
+            // Answer validation
+            string[] words = psbword.Split(' ');
+            var result = words.Where(w => word.All(w.Contains));
+
+            return Extensions.LengthIEnumerable(result) > 0;
+        }
+    }
+
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Countdown
-A terminal version of the British game-show Countdown made in C#
+
+<div align="center"><h2>
+    A terminal version of the British game-show Countdown made in C#<br/></br>Made by Raiyaxi Ziaoi
+</h2></div>
